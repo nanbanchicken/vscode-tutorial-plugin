0a962a919edd877dc225d548633b9f51377d15d4
NishinoJunji <nishinojunji@uec.ac.jp>
2023-08-12 12:54:32 +0900

    １１４回 hello world エントリーを削除
    Co-authored-by: NanbanChicken@outlook.jp <NanbanChicken@outlook.jp>

--- src/extension.ts
+++ src/extension.ts
@@ -15,20 +15,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// // ミラー
 	// let nottubedhsup;
 
-	// let disposable = vscode.commands.registerCommand('vscode-tutorial-plugin.helloWorld', () => {
-	// 	const pushed_button = vscode.window.showInformationMessage('Hello World from vscode_tutorial_plugin!', "tekitou", "mouikko");
-	// 	pushed_button.then((value) => {
-	// 		// コンソールログに押下したボタン名（Button1 or Button2）が表示されます。
-	// 		console.log(value);
-
-	// 		if(value === "tekitou"){  
-	// 			console.log("hazure!");
-	// 		}
-	// 	  });
-	// });
-
-	// context.subscriptions.push(disposable);
-
 	// 補完機能を push
     // const provider = vscode.languages.registerCompletionItemProvider('plaintext', new Completions(), '.');
 	const provider = vscode.languages.registerCompletionItemProvider('plaintext', new Completions());
